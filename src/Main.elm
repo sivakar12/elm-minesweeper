983cd4d74db5f8be51ca88d366cf795c0d0a0ced
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-10-06 23:20:03 +0530

    Add event handling to uncover on click

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,52 @@
 module Main exposing (..)
 import Browser
 import Html exposing (Html, div, text, span)
+import Html.Events exposing (onClick)
 
+-- TYPES
 type GameStatus = 
   NotStart |
   Playing |
   Finished
 
 type alias Cell = { covered: Bool, mine: Bool }
-  
-type alias Model = { grid: List (List Cell) }
+type alias Row = List Cell
+type alias Grid = List Row
+type alias Model = { grid: Grid }
 
 type Msg = 
   OpenCell { x: Int, y: Int } |
   FlagCell { x: Int, y: Int }
 
-update: Msg -> Model -> Model
-update _ model = 
-  model
 
-displayCell: Cell -> Html Msg
-displayCell { covered, mine } =
+-- STATE UPDATE HANDLERS
+
+openCellOnGridLocation: Grid -> Int -> Int -> Grid
+openCellOnGridLocation grid x y =
+  List.indexedMap (\i row -> ( List.indexedMap (\j cell -> if (i == x && j == y && cell.covered) then { cell | covered = False} else cell) row)) grid
+update: Msg -> Model -> Model
+update msg model = 
+  case msg of
+     OpenCell cell -> {model | grid = (openCellOnGridLocation model.grid cell.x cell.y)}
+     _ -> model
+
+
+-- VIEWS
+displayCell: Int -> Int -> Cell -> Html Msg
+displayCell x y { covered, mine } =
 
   case (covered, mine) of
-    (True, _) -> span [] [text "📦"]
+    (True, _) -> span [ onClick (OpenCell{x = x, y = y})] [text "📦"]
     (False, True) -> span [] [text "💣"]
     (False, False) -> span [] [text "8"] -- calculate and put actual number
 
-displayRow: List Cell -> Html Msg
-displayRow row =
-  div [] (List.map displayCell row)
+displayRow: Int -> List Cell -> Html Msg
+displayRow x row =
+  div [] (List.indexedMap (\y cell -> displayCell x y cell) row)
 
 displayGrid: List (List Cell) -> Html Msg
 displayGrid grid =
-  div [] (List.map displayRow grid)
+  div [] (List.indexedMap displayRow grid)
 
 view: Model -> Html Msg
 view model =
@@ -43,9 +56,13 @@ view model =
     displayGrid model.grid
   ]
 
+
+-- INITIAL STATE
 initialGrid: Int -> Int -> List (List Cell)
 initialGrid rows columns = List.repeat rows (List.repeat columns (Cell True False) )
 
 initialModel : Model
 initialModel = Model (initialGrid 10 10)
+
+-- START
 main = Browser.sandbox { init = initialModel, update = update, view = view}
