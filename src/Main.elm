ace21854bdaf16d001dc22ef7e390d290447b90f
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-10-08 23:09:12 +0530

    Make changing grid shape work in elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,10 @@ import Random.Set
 import Grid exposing (Grid)
 import Element as E
 import Element.Font
+import Element.Input
+import Element.Border
 import Maybe
+import Array
 
 -- TYPES
 type GameStatus = 
@@ -27,16 +30,19 @@ type MineState
 type alias Cell = { covered: CoverState, mine: MineState }
 type alias Model = { grid: Grid Cell }
 
+type ChangeAxis = Width | Height
+type ChangeDirection = Increase | Decrease
+
 type alias BombPositions = Set (Int, Int)
 type Msg 
   = OpenCell { x: Int, y: Int }
   | FlagCell { x: Int, y: Int }
   | AddBombs BombPositions
-  | HandleRowInputChange String
-  | HandleColumnInputChange String
+  | ChangeGridSize ChangeAxis ChangeDirection
 
 
 -- STATE UPDATE HANDLERS
+defaultCell: Cell
 defaultCell = Cell Covered NotMined
 
 openCellOnGridLocation: Grid Cell -> Int -> Int -> Grid Cell
@@ -73,37 +79,65 @@ update msg model =
         newGrid = Set.foldl (\(x, y) grid -> (placeBombOnGridLocation grid x y)) model.grid bombLocations
         newModel = { model | grid = newGrid }
       in (newModel, Cmd.none)
-    HandleRowInputChange input -> 
+    ChangeGridSize axis direction ->
       let
-          newRows = Maybe.withDefault initialRows (String.toInt input)
-          newColumns = Grid.height model.grid
-          newBombs = 20
-          newCommand = initialCommand newRows newColumns newBombs
-          newGrid = initialGrid newRows newColumns
-          newModel = { model | grid = newGrid}
-      in
-      (newModel, newCommand)
-    HandleColumnInputChange input -> 
-      let
-          newColumns = Maybe.withDefault initialRows (String.toInt input)
-          newRows = Grid.width model.grid
-          newBombs = 20
-          newCommand = initialCommand newRows newColumns newBombs
-          newGrid = initialGrid newRows newColumns
+          change = 
+            case direction of
+              Increase -> 1
+              Decrease -> -1
+          oldWidth = Grid.width model.grid
+          oldHeight = Grid.height model.grid
+          newWidth = 
+            case axis of 
+              Width -> oldWidth + change
+              Height -> oldWidth
+          newHeight = 
+            case axis of
+              Width -> oldHeight
+              Height -> oldHeight + change
+          newBombs = 10 -- Todo: Change to 0.2 * new rows * new columns
+          newCommand = initialCommand newWidth newHeight newBombs
+          newGrid = initialGrid newWidth newHeight
           newModel = { model | grid = newGrid}
       in
       (newModel, newCommand)
 
-
+-- there are as many rows as height in grid
 -- VIEWS
 
+displayCell: Int -> Int -> Cell -> E.Element Msg
+displayCell x y cell =
+  E.text "O"
+
+displayRow: Int -> Array.Array Cell -> E.Element Msg
+displayRow x row =
+  E.row
+    []
+    <| Array.toList <| Array.indexedMap (\y cell -> (displayCell x y cell)) <| row
 gridView: Grid Cell -> E.Element Msg
 gridView grid =
-  E.text "Grid"
+  E.column
+    []
+    <| Array.toList <| Array.indexedMap displayRow <| Grid.rows grid
 
 controlsView: E.Element Msg
 controlsView =
-  E.text "Controls"
+  let
+    controlButtonStyles =
+      [ Element.Border.width 1
+      -- , Element.Border.rounded 50
+      ]
+  in
+  E.row
+    []
+    [
+      E.text "Width: ",
+      Element.Input.button controlButtonStyles { label = E.text "➕", onPress = Maybe.Just <| ChangeGridSize Width Increase },
+      Element.Input.button controlButtonStyles { label = E.text "➖", onPress = Maybe.Just <| ChangeGridSize Width Decrease },
+      E.text "Height: ",
+      Element.Input.button controlButtonStyles { label = E.text "➕", onPress = Maybe.Just <| ChangeGridSize Height Increase },
+      Element.Input.button controlButtonStyles { label = E.text "➖", onPress = Maybe.Just <| ChangeGridSize Height Decrease } 
+    ]
 
 view: Model -> Html Msg
 view model = 
@@ -119,29 +153,29 @@ view model =
           E.height E.fill
       ] 
       [
-        E.el [E.alignTop, Element.Font.size 50] <| E.text "Minesweeper",
-        gridView model.grid,
-        E.el [E.alignBottom] controlsView
+        E.el [E.alignTop, E.centerX, Element.Font.size 50] <| E.text "Minesweeper",
+        E.el [E.centerX, E.centerY] <| gridView model.grid,
+        E.el [E.alignBottom, E.centerX] controlsView
       ]
 
 
 -- INITIAL STATE
-initialRows = 10
-initialColumns = 6
+initialWidth = 10
+initialHeight = 6
 initialBombs = 20
 
 getRandomBombPositions: Int -> Int -> Int -> Random.Generator BombPositions
-getRandomBombPositions rows columns bombs = 
-  Random.Set.set bombs (Random.pair (Random.int 0 rows) (Random.int 0 columns))
+getRandomBombPositions width height bombs = 
+  Random.Set.set bombs (Random.pair (Random.int 0 height) (Random.int 0 width))
 
 initialGrid: Int -> Int -> Grid Cell
-initialGrid rows columns = 
-  Grid.repeat rows columns defaultCell
+initialGrid width height = 
+  Grid.repeat width height defaultCell
 
-initialCommand rows columns bombs = Random.generate AddBombs (getRandomBombPositions rows columns bombs)
+initialCommand width height bombs = Random.generate AddBombs (getRandomBombPositions width height bombs)
 
 init : () -> (Model, Cmd Msg)
-init _ = (Model (initialGrid initialRows initialColumns), initialCommand initialRows initialColumns initialBombs)
+init _ = (Model (initialGrid initialWidth initialHeight), initialCommand initialWidth initialHeight initialBombs)
 
 subscriptions : Model -> Sub Msg
 subscriptions _ = Sub.none
