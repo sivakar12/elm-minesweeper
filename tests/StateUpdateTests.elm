23f5cc9dffa5bde6208edfe4666e8ddcfab7a87f
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-10-17 22:56:19 +0530

    Add one test

---
+++ tests/StateUpdateTests.elm
@@ -0,0 +1,30 @@
+module StateUpdateTests exposing (suite)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+import Grid
+import Types exposing (..)
+import StateUpdate exposing (createEmptyGrid)
+import Maybe exposing (withDefault)
+
+suite: Test
+suite =
+  describe "The StateUpdate module" 
+    [ describe "createEmptyGrid function"
+      [ test "returns a grid with the exact size" <|
+          \_ ->
+            let
+              defaultCell = Cell Covered NotMined 0
+              expected = 
+                Grid.fromList 
+                  [ [ defaultCell, defaultCell, defaultCell ]
+                  , [ defaultCell, defaultCell, defaultCell ]
+                  , [ defaultCell, defaultCell, defaultCell ]
+                  ]
+              returned = createEmptyGrid 3 3
+            in
+              Expect.equal expected (Maybe.Just returned)
+      ]
+    ]
