bc2da002bb8c917f501d07b2a363ae0a469e73ad
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-11-24 20:09:25 +0530

    Add another test

--- tests/StateUpdateTests.elm
+++ tests/StateUpdateTests.elm
@@ -6,7 +6,11 @@ import Test exposing (..)
 
 import Grid
 import Types exposing (..)
-import StateUpdate exposing (createEmptyGrid)
+import StateUpdate exposing 
+  ( createEmptyGrid
+  , getBombCountFromGridSize
+  -- , getRandomBombPositions
+  )
 import Maybe
 
 suite: Test
@@ -26,5 +30,25 @@ suite =
               returned = createEmptyGrid 3 3
             in
               Expect.equal expected (Maybe.Just returned)
+      ],
+      describe "getBombCountFromGridSize function"
+      [
+        test "returns the correct number of bombs" <|
+          \_ ->
+            let
+              returned = getBombCountFromGridSize 11 13
+            in
+              Expect.equal 29 returned
       ]
+      -- describe "getRandomBombPositions function"
+      -- [
+      --   test "returns the correct number of positions" <|
+      --     \_ ->
+      --       let
+      --         generator = getRandomBombPositions 10 10 10
+
+      --       in
+      --         Expect.equal 10 returned.length
+      -- ]
+      -- ] 
     ]
